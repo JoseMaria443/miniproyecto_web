fe01aaaec8683fd71883938ad5a26a7030c23497
JoseMaria443 <243752@ids.upchiapas.edu.mx>
2026-02-04 21:44:45 -0600

    istas 5 creada y solucion de github

--- db/view5.sql
+++ db/view5.sql
@@ -1,1 +1,24 @@
--- Viewasdas
+-- vw_rank_studen
+-- Que devuelve: El ranking de estudiantes por promedio de calificaciones en un periodo.
+-- Grain: 1 fila por estudiante + programa + periodo
+-- Metricas: Ranking academico de los programas 
+
+CREATE OR REPLACE VIEW vw_rank_student AS
+SELECT
+    e.nombre AS estudiante_nombre,
+    e.programa AS programa,
+    e.periodo AS termino,
+    ROUND(AVG(cal.calificacion), 2) AS promedio_calificaciones,
+    RANK() OVER(
+        PARTITION BY e.programa, e.periodo
+        ORDER BY AVG(cal.calificacion) DESC
+    ) AS posicion_en_ranking
+    ROW_NUMBER() OVER(
+        PARTITION BY e.programa, e.periodo
+        ORDER BY AVG(cal.calificacion) DESC
+    )
+    FROM Estudiantes e
+    JOIN Inscripciones i ON e.id = i.estudiante_id
+    JOIN Grupos g ON i.grupo_id = g.id
+    JOIN Calificaciones cal ON i.id = cal.inscripcion_id
+    GROUP BY e.id, e.nombre, e.programa, e.periodo;
